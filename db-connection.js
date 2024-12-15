b4bcb6789b088b1bd63eb94f50c116f5b01c7f79
adeliya21 <adeliya.salieva@gmail.com>
2024-12-14 19:38:28 -0800

    remove &appName=Cluster0 in DB

--- db-connection.js
+++ db-connection.js
@@ -1,7 +1,21 @@
 const mongoose = require("mongoose")
-const db = mongoose.connect(process.env.DB, {
-    useUnifiedTopology: true,
-    userNewUrlParser: true,
-})
+
+const db = async () => {
+    try {
+      await mongoose.connect(process.env.DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Successfully connected to the database.");
+    } catch (error) {
+      console.error("Error connecting to the database:", error.message);
+      process.exit(1); // Exit the process with an error code
+    }
+  };
+
+// const db = mongoose.connect(process.env.DB, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+// })
 
 module.exports = db
